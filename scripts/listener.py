ed3bb15c3067113fd8231fd2e09626b1b39aef86
Theo LUBERT <theo.lubert@econocom.com>
2014-04-05 20:04:06 +0200

    ' character fix

--- scripts/listener.py
+++ scripts/listener.py
@@ -4,6 +4,8 @@ import os
 import signal
 import json
 
+TIMEOUT = 20
+
 def handler(signum, frame):
     print 'Signal handler called with signal', signum
     raise IOError("Couldn't open device!")
@@ -15,8 +17,8 @@ signal.signal(15, handler)
 last_verified = None
 last_id = None
 while True:
-	os.popen("scripts/timeout -s 15 -t 10 scripts/record.sh 2>&1 >> /dev/null")
-	os.popen("killall -9 sox")
+	os.popen("scripts/timeout -s 15 -t %d scripts/record.sh 2>&1 >> /dev/null" % TIMEOUT)
+	os.popen("sleep 1 && killall -9 sox")
 	
 	result = os.popen('export ECHO_NEST_API_KEY=XEL1XZ9B6LIRBKVWP && scripts/lookup.py /tmp/audio.wav').read()
 	data = json.loads(result)
@@ -27,5 +29,5 @@ while True:
 			last_verified = last_id
 
 			print "New song playing: ", result
-			os.popen('osascript -e \'display notification "%s" with title "%s"\'' % (data["song"]["title"], data["artist"]["name"]))
+			os.popen('osascript -e \'display notification "%s" with title "%s"\'' % (data["song"]["title"].replace("'", " "), data["artist"]["name"].replace("'", " ")))
 		last_id = data["song"]["id"]
